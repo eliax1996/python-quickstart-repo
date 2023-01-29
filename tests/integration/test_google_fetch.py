ff6e1a882a6538f7d16440cbee3e9621a98dcae1
eliax1996 <elia@leadiq.com>
2023-01-29 18:42:52 +0100

    wip

--- tests/integration/test_google_fetch.py
+++ tests/integration/test_google_fetch.py
@@ -21,6 +21,19 @@ from python_quickstart_repo.status_writer import KafkaFetchProducer
 from tests.unit.mocked_helpers import CollectorConsumer, MockedAsyncFetcher
 
 
+@pytest.mark.asyncio
+async def test_fetch_google():
+    page_fetcher = AsyncHttpFetcher("https://www.google.com/", 1, re.compile(".*google.*"))
+    reply_count = 0
+
+    async for reply in aiostream.stream.take(page_fetcher, 3):
+        reply_count += 1
+        assert reply.status_code == 200
+        assert reply.regex_match is True
+        assert reply.url == "https://www.google.com/"
+
+    assert reply_count == 3
+
 
 @pytest.mark.asyncio
 async def test_writing_and_reading_from_kafka():
